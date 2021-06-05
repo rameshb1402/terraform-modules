bf33434949c7061da061e1c041c2b5773f54bd98
kalyan <kaleonx@tutanota.com>
2021-06-05 17:13:28 +0530

    key_alias

--- aws/kms/kms_alias/variables.tf
+++ aws/kms/kms_alias/variables.tf
@@ -1,8 +1,6 @@
 variable "key_alias" {
   description = "Name of the aws service. Eg: s3, ebs, rds etc."
-  default = ""
 }
 variable "kms_key_id" {
   description = "Policy file that has to be attached"
-  default = ""
 }
