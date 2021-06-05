7780a6301f9f0ca7b436be1d2a277719ee37b193
kalyan <kaleonx@tutanota.com>
2021-06-05 18:25:59 +0530

    disabling aws_api_gateway_vpc_link

--- aws/api-gateway/outputs.tf
+++ aws/api-gateway/outputs.tf
@@ -4,6 +4,6 @@ output "id" {
 output "name" {
   value = aws_api_gateway_rest_api.api_gw.name
 }
-output "vpc_link_id" {
-  value = aws_api_gateway_vpc_link.api_vpc_link.id
-}
+#output "vpc_link_id" {
+#  value = aws_api_gateway_vpc_link.api_vpc_link.id
+#}
