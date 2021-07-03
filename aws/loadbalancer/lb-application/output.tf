e5aa348711f5da62828c0ba80625ec5e1d2a99c2
kalyan <kaleonx@tutanota.com>
2021-07-03 14:02:49 +0530

    alb o/p id to arn and arn to id

--- aws/loadbalancer/lb-application/output.tf
+++ aws/loadbalancer/lb-application/output.tf
@@ -1,8 +1,8 @@
 output "alb_id" {
-  value = aws_lb.alb.arn
+  value = aws_lb.alb.id
 }
 output "alb_arn" {
-  value = aws_lb.alb.id
+  value = aws_lb.alb.arn
 }
 output "alb_arn_suffix" {
   value = aws_lb.alb.arn_suffix
